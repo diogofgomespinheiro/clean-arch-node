d6f4b65a19199ee6be8a680449373da567ca7e9d
Diogo Pinheiro <diogo.fgomes.pinheiro@gmail.com>
2020-10-08 00:13:19 +0100

    test(db-add-account): ensure dbaddaccount throws if encrypter throws

--- src/data/useCases/add-account/db-add-account.spec.ts
+++ src/data/useCases/add-account/db-add-account.spec.ts
@@ -37,4 +37,22 @@ describe('DbAddAccount Usecase', () => {
     await sut.add(accountData);
     expect(encryptSpy).toHaveBeenCalledWith(accountData.password);
   });
+
+  it('Should throw if Encrypter throws', async () => {
+    const { sut, encrypterStub } = makeSut();
+    jest
+      .spyOn(encrypterStub, 'encrypt')
+      .mockReturnValueOnce(
+        new Promise((resolve, reject) => reject(new Error()))
+      );
+
+    const accountData = {
+      name: 'valid_name',
+      email: 'valid_email@mail.com',
+      password: 'valid_password'
+    };
+
+    const promise = sut.add(accountData);
+    await expect(promise).rejects.toThrow();
+  });
 });
