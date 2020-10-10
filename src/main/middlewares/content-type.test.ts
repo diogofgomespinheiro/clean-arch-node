249ff7a89cb7e815d1e79d6998321db0d9eb2bd3
Diogo Pinheiro <diogo.fgomes.pinheiro@gmail.com>
2020-10-10 18:34:08 +0100

    test(main): ensure request returns json as default content type

---
+++ src/main/middlewares/content-type.test.ts
@@ -0,0 +1,25 @@
+import request from 'supertest';
+import { Request, Response } from 'express';
+
+import app from '../config/app';
+
+describe('Content Type Middleware', () => {
+  it('Should return default content as type json', async () => {
+    app.get('/test_content_type', (req: Request, res: Response) => {
+      res.send('');
+    });
+
+    await request(app).get('/test_content_type').expect('content-type', /json/);
+  });
+
+  it('Should return xml content when forced', async () => {
+    app.get('/test_content_type_xml', (req: Request, res: Response) => {
+      res.type('xml');
+      res.send('');
+    });
+
+    await request(app)
+      .get('/test_content_type_xml')
+      .expect('content-type', /xml/);
+  });
+});
