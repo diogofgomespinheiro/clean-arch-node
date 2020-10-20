3838e8b7e8926ff70fc60e7698286093d668bb46
Diogo Pinheiro <diogo.fgomes.pinheiro@gmail.com>
2020-10-20 09:09:41 +0100

    refactor(db-authentication): remove duplicated code to helper methods

--- src/data/useCases/authentication/db-authentication.spec.ts
+++ src/data/useCases/authentication/db-authentication.spec.ts
@@ -1,29 +1,54 @@
 import { LoadAccountByEmailRepository } from '@/data/protocols/load-account-by-email-repository';
 import { AccountModel } from '@/domain/models/account';
+import { AuthenticationModel } from '@/domain/useCases/authentication';
 import { DbAuthentication } from './db-authentication';
+
+const makeFakeAccount = (): AccountModel => ({
+  id: 'any_id',
+  name: 'any_name',
+  email: 'any_email@mail.com',
+  password: 'any_password'
+});
+
+const makeFakeAuthentication = (): AuthenticationModel => ({
+  email: 'any_email@mail.com',
+  password: 'any_password'
+});
+
+interface SutTypes {
+  sut: DbAuthentication;
+  loadAccountByEmailRepositoryStub: LoadAccountByEmailRepository;
+}
+
+const makeLoadAccountByEmailRepository = (): LoadAccountByEmailRepository => {
+  class LoadAccountByEmailRepositoryStub
+    implements LoadAccountByEmailRepository {
+    async load(email: string): Promise<AccountModel> {
+      return makeFakeAccount();
+    }
+  }
+
+  return new LoadAccountByEmailRepositoryStub();
+};
+
+const makeSut = (): SutTypes => {
+  const loadAccountByEmailRepositoryStub = makeLoadAccountByEmailRepository();
+  const sut = new DbAuthentication(loadAccountByEmailRepositoryStub);
+
+  return {
+    sut,
+    loadAccountByEmailRepositoryStub
+  };
+};
 
 describe('DbAuthentication Use Case', () => {
   it('should call LoadAccountByEmailRepostiory with correct email', async () => {
-    class LoadAccountByEmailRepositoryStub
-      implements LoadAccountByEmailRepository {
-      async load(email: string): Promise<AccountModel> {
-        const account: AccountModel = {
-          id: 'any_id',
-          name: 'any_name',
-          email: 'any_email@mail.com',
-          password: 'any_password'
-        };
-
-        return account;
-      }
-    }
-    const loadAccountByEmailRepositoryStub = new LoadAccountByEmailRepositoryStub();
-    const sut = new DbAuthentication(loadAccountByEmailRepositoryStub);
-
+    const { sut, loadAccountByEmailRepositoryStub } = makeSut();
     const loadSpy = jest.spyOn(loadAccountByEmailRepositoryStub, 'load');
 
-    await sut.auth({ email: 'any_email@mail.com', password: 'any_password' });
+    const fakeAuthentication = makeFakeAuthentication();
+    await sut.auth(fakeAuthentication);
 
-    expect(loadSpy).toHaveBeenCalledWith('any_email@mail.com');
+    expect(loadSpy).toHaveBeenCalledWith(fakeAuthentication.email);
   });
 });
