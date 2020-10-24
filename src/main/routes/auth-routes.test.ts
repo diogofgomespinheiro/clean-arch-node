a6a5398709416e48b5da7aa4fdf96b6ed5091f21
Diogo Pinheiro <diogo.fgomes.pinheiro@gmail.com>
2020-10-24 17:17:17 +0100

    test(main): ensure Login returns 200 on success

--- src/main/routes/auth-routes.test.ts
+++ src/main/routes/auth-routes.test.ts
@@ -1,6 +1,9 @@
 import request from 'supertest';
 
 import app from '@/main/config/app';
+import { MongoHelper } from '@/infra/db/mongodb/helpers/mongo-helper';
+import { hash } from 'bcrypt';
+import env from '@/main/config/env';
 
 describe('Auth Routes', () => {
   describe('POST /signup', () => {
@@ -12,6 +15,26 @@ describe('Auth Routes', () => {
           email: 'diogo@gmail.com',
           password: '123456',
           passwordConfirmation: '123456'
+        })
+        .expect(200);
+    });
+  });
+
+  describe('POST /login', () => {
+    it('should return 200 on login', async () => {
+      const accountCollection = await MongoHelper.getCollection('accounts');
+      const password = await hash('123456', env.defaultSalt);
+      await accountCollection.insertOne({
+        name: 'Diogo',
+        email: 'diogo@gmail.com',
+        password
+      });
+
+      await request(app)
+        .post('/api/v1/login')
+        .send({
+          email: 'diogo@gmail.com',
+          password: '123456'
         })
         .expect(200);
     });
