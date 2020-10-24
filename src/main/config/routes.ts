d08274d5af601b4104bc928d857e5077883ecba5
Diogo Pinheiro <diogo.fgomes.pinheiro@gmail.com>
2020-10-24 18:31:24 +0100

    refactor: change method to import routes

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -1,10 +1,18 @@
 import { Express, Router } from 'express';
 import fg from 'fast-glob';
+import path from 'path';
 
 export default (app: Express): void => {
   const router = Router();
+  const routesPath = path.join(__dirname, '..', 'routes');
+
   app.use('/api/v1', router);
-  fg.sync('**/src/main/routes/**routes.ts').map(async file => {
-    (await import(`../../../${file}`)).default(router);
+
+  fg.sync(`${routesPath}/**routes.+(js|ts)`, {
+    extglob: true,
+    braceExpansion: true,
+    unique: true
+  }).map(async file => {
+    (await import(file)).default(router);
   });
 };
