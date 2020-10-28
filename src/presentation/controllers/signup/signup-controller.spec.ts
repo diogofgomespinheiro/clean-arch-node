0b133cd885604caf3df0dd165a894b1aa84fba35
Diogo Pinheiro <diogo.fgomes.pinheiro@gmail.com>
2020-10-28 22:15:37 +0000

    feat(presentation): ensure SignUpController return accessToken on success

--- src/presentation/controllers/signup/signup-controller.spec.ts
+++ src/presentation/controllers/signup/signup-controller.spec.ts
@@ -121,7 +121,7 @@ describe('SignUp Controller', () => {
     const httpRequest = makeFakeRequest();
 
     const httpResponse = await sut.handle(httpRequest);
-    expect(httpResponse).toEqual(ok(makeFakeAccount()));
+    expect(httpResponse).toEqual(ok({ accessToken: 'any_token' }));
   });
 
   it('should call Validation with correct value', async () => {
