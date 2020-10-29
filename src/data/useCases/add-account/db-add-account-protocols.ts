6ff55ef506de1d48ff731806351b3350e22de88f
Diogo Pinheiro <diogo.fgomes.pinheiro@gmail.com>
2020-10-29 09:00:50 +0000

    feat(data): ensure DbAddAccount calls LoadAccountByEmailRepository with correct email

--- src/data/useCases/add-account/db-add-account-protocols.ts
+++ src/data/useCases/add-account/db-add-account-protocols.ts
@@ -2,3 +2,4 @@ export * from '@/domain/models/account';
 export * from '@/domain/useCases/add-account';
 export * from '@/data/protocols/crytography/hasher';
 export * from '@/data/protocols/db/account/add-account-repository';
+export * from '@/data/protocols/db/account/load-account-by-email-repository';
