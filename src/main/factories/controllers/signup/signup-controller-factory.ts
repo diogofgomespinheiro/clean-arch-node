ce49c678544db133e3395369d604fe5613ea247a
Diogo Pinheiro <diogo.fgomes.pinheiro@gmail.com>
2020-10-31 16:11:59 +0000

    refactor(presentation): move signup and login controllers to auth folder

--- src/main/factories/controllers/signup/signup-controller-factory.ts
+++ src/main/factories/controllers/signup/signup-controller-factory.ts
@@ -2,7 +2,7 @@ import { Controller } from '@/presentation/protocols';
 import { makeDbAuthentication } from '../../useCases/authentication/db-authentication-factory';
 import { makeDbAddAccount } from '../../useCases/add-account/db-add-account-factory';
 import { makeSignUpValidation } from './signup-validation-factory';
-import { SignUpController } from '@/presentation/controllers/signup/signup-controller';
+import { SignUpController } from '@/presentation/controllers/auth/signup/signup-controller';
 import { makeLogControllerDecorator } from '../../decorators/log-controller-decorator-factory';
 
 export const makeSignUpController = (): Controller => {
