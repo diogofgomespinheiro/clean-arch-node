483a45c44de662cace00e82b8d345372e86a124e
Diogo Pinheiro <diogo.fgomes.pinheiro@gmail.com>
2020-10-31 17:16:09 +0000

    feat(presentation): ensure AddSurveyController returns 204 on success

--- src/presentation/controllers/survey/add-survey/add-survey-controller.ts
+++ src/presentation/controllers/survey/add-survey/add-survey-controller.ts
@@ -1,5 +1,6 @@
 import {
   badRequest,
+  noContent,
   serverError
 } from '@/presentation/helpers/http/http-helper';
 import {
@@ -31,7 +32,7 @@ export class AddSurveyController implements Controller {
         answers
       });
 
-      return null;
+      return noContent();
     } catch (error) {
       return serverError(error);
     }
