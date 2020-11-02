c6c7fa325d29c67ec6e9a3a245173fa0d0d305eb
Diogo Pinheiro <diogo.fgomes.pinheiro@gmail.com>
2020-11-02 22:03:46 +0000

    test(infra): ensure accountMongoRepo returns an account on find by token

--- src/infra/db/mongodb/account/account-mongo-repository.spec.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.spec.ts
@@ -3,10 +3,15 @@ import { MongoHelper } from '../helpers/mongo-helper';
 import { Collection } from 'mongodb';
 import { AccountMongoRepository } from './account-mongo-repository';
 
-const makeFakeAccountData = (): AddAccountModel => ({
+const makeFakeAccountData = (
+  accessToken?: string,
+  role?: string
+): AddAccountModel => ({
   name: 'any_name',
   email: 'any_email@mail.com',
-  password: 'any_password'
+  password: 'any_password',
+  ...(accessToken && { accessToken }),
+  ...(role && { role })
 });
 
 const makeAccountCollection = async (): Promise<Collection> => {
@@ -78,6 +83,42 @@ describe('Account Mongo Repository', () => {
 
       expect(account).toBeTruthy();
       expect(account.accessToken).toBe('any_token');
+    });
+  });
+
+  describe('loadByToken()', () => {
+    it('should return an account on loadByToken success without role', async () => {
+      const sut = makeSut();
+
+      const accountData = makeFakeAccountData('any_token', 'any_role');
+
+      const accountCollection = await makeAccountCollection();
+      await accountCollection.insertOne(accountData);
+
+      const account = await sut.loadByToken('any_token', 'any_role');
+
+      expect(account).toBeTruthy();
+      expect(account.id).toBeTruthy();
+      expect(account.name).toBe(accountData.name);
+      expect(account.email).toBe(accountData.email);
+      expect(account.password).toBe(accountData.password);
+    });
+
+    it('should return an account on loadByToken success with role', async () => {
+      const sut = makeSut();
+
+      const accountData = makeFakeAccountData('any_token');
+
+      const accountCollection = await makeAccountCollection();
+      await accountCollection.insertOne(accountData);
+
+      const account = await sut.loadByToken('any_token');
+
+      expect(account).toBeTruthy();
+      expect(account.id).toBeTruthy();
+      expect(account.name).toBe(accountData.name);
+      expect(account.email).toBe(accountData.email);
+      expect(account.password).toBe(accountData.password);
     });
   });
 });
