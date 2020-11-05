119d30191995b82ce38be8a37b3f429e72069ad5
Diogo Pinheiro <diogo.fgomes.pinheiro@gmail.com>
2020-11-05 09:10:06 +0000

    feat(presentation): ensure LoadSurveysController returns 204 if LoadSurveys returns an empty array

--- src/presentation/controllers/survey/load-surveys/load-surveys-controller.ts
+++ src/presentation/controllers/survey/load-surveys/load-surveys-controller.ts
@@ -1,4 +1,8 @@
-import { ok, serverError } from '@/presentation/helpers/http/http-helper';
+import {
+  noContent,
+  ok,
+  serverError
+} from '@/presentation/helpers/http/http-helper';
 import {
   Controller,
   HttpRequest,
@@ -12,7 +16,7 @@ export class LoadSurveysController implements Controller {
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
       const surveys = await this.loadSurveys.load();
-      return ok(surveys);
+      return surveys.length ? ok(surveys) : noContent();
     } catch (error) {
       return serverError(error);
     }
