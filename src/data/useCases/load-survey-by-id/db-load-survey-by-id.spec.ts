4ef563d170f28bd2331eb4c969b215b7e617c0c3
Diogo Pinheiro <diogo.fgomes.pinheiro@gmail.com>
2020-11-08 01:07:12 +0000

    test(data): ensure DbLoadSurveyById throws if LoadSurveyByIdRepo throws

--- src/data/useCases/load-survey-by-id/db-load-survey-by-id.spec.ts
+++ src/data/useCases/load-survey-by-id/db-load-survey-by-id.spec.ts
@@ -59,4 +59,16 @@ describe('DbLoadSurveyById', () => {
     const survey = await sut.loadById('any_id');
     expect(survey).toEqual(makeFakeSurvey());
   });
+
+  it('should throw if LoadSurveyByIdRepository throws', async () => {
+    const { sut, loadSurveyByIdRepositoryStub } = makeSut();
+    jest
+      .spyOn(loadSurveyByIdRepositoryStub, 'loadById')
+      .mockImplementationOnce(async () => {
+        throw new Error();
+      });
+
+    const promise = sut.loadById('any_id');
+    await expect(promise).rejects.toThrow();
+  });
 });
