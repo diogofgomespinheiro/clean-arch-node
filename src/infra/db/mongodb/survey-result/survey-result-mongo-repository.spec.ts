f18da2161b9bc612af45c41275e3c189ef3461d2
Diogo Pinheiro <diogo.fgomes.pinheiro@gmail.com>
2020-11-09 21:24:57 +0000

    test(infra): ensure SurveyResultMongoRepository inserts new surveyResult
    ensure SurveyResultMongoRepository inserts new surveyResult if there is none with that data

---
+++ src/infra/db/mongodb/survey-result/survey-result-mongo-repository.spec.ts
@@ -0,0 +1,71 @@
+import { AddSurveyModel } from '@/domain/useCases/add-survey';
+import { MongoHelper } from '@/infra/db/mongodb/helpers/mongo-helper';
+import { SaveSurveyResultModel } from '@/domain/useCases/save-survey-result';
+import { SurveyModel } from '@/domain/models/survey';
+import { AccountModel } from '@/domain/models/account';
+import { AddAccountModel } from '@/domain/useCases/add-account';
+import { SurveyResultMongoRepository } from './survey-result-mongo-repository';
+
+const makeFakeSurveyData = (question = 'any_question'): AddSurveyModel => ({
+  question,
+  answers: [
+    {
+      image: 'any_image',
+      answer: 'any_answer'
+    },
+    {
+      answer: 'other_answer'
+    }
+  ],
+  date: new Date()
+});
+
+const makeFakeAccountData = (): AddAccountModel => ({
+  name: 'valid_name',
+  email: 'valid_email@mail.com',
+  password: 'valid_password'
+});
+
+const makeFakeSurveyResultData = (
+  account: AccountModel,
+  survey: SurveyModel
+): SaveSurveyResultModel => ({
+  surveyId: survey.id,
+  accountId: account.id,
+  answer: survey.answers[0].answer,
+  date: new Date()
+});
+
+const makeSurvey = async (): Promise<SurveyModel> => {
+  const surveyCollection = await MongoHelper.getCollection('surveys');
+  const res = await surveyCollection.insertOne(makeFakeSurveyData());
+  return res.ops[0];
+};
+
+const makeAccount = async (): Promise<AccountModel> => {
+  const accountCollection = await MongoHelper.getCollection('accounts');
+  const res = await accountCollection.insertOne(makeFakeAccountData());
+  return res.ops[0];
+};
+
+const makeSut = (): SurveyResultMongoRepository => {
+  return new SurveyResultMongoRepository();
+};
+
+describe('Survey Result Mongo Repository', () => {
+  describe('save()', () => {
+    it('should add a survey result if it`s new', async () => {
+      const survey = await makeSurvey();
+      const account = await makeAccount();
+      const sut = makeSut();
+
+      const surveyResult = await sut.save(
+        makeFakeSurveyResultData(account, survey)
+      );
+
+      expect(surveyResult).toBeTruthy();
+      expect(surveyResult.id).toBeTruthy();
+      expect(surveyResult.answer).toBe(survey.answers[0].answer);
+    });
+  });
+});
