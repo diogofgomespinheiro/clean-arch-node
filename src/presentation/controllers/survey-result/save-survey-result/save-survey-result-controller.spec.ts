6f183067a0794bedc7caca31fa42babf91a3155d
Diogo Pinheiro <diogo.fgomes.pinheiro@gmail.com>
2020-11-10 22:37:14 +0000

    test(presentation): ensure SaveSurveyResultController returns 500 if LoadSurveyById throws

--- src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.spec.ts
+++ src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.spec.ts
@@ -4,6 +4,11 @@ import {
   SurveyModel
 } from './save-survey-result-protocols';
 import { SaveSurveyResultController } from './save-survey-result-controller';
+import {
+  forbidden,
+  serverError
+} from '@/presentation/helpers/http/http-helper';
+import { InvalidParamError, ServerError } from '@/presentation/errors';
 
 const makeFakeRequest = (): HttpRequest => ({
   params: {
@@ -53,5 +58,28 @@ describe('SaveSurveyResult Controller', () => {
     const httpRequest = makeFakeRequest();
     await sut.handle(httpRequest);
     expect(loadByIdSpy).toHaveBeenCalledWith(httpRequest.params.surveyId);
+  });
+
+  it('should return 403 if LoadSurveyById returns null', async () => {
+    const { sut, loadSurveyByIdStub } = makeSut();
+    jest
+      .spyOn(loadSurveyByIdStub, 'loadById')
+      .mockImplementationOnce(async () => null);
+
+    const httpResponse = await sut.handle(makeFakeRequest());
+    expect(httpResponse).toEqual(forbidden(new InvalidParamError('surveyId')));
+  });
+
+  it('should return 500 if LoadSurveys throws an exception', async () => {
+    const { sut, loadSurveyByIdStub } = makeSut();
+
+    jest.spyOn(loadSurveyByIdStub, 'loadById').mockImplementationOnce(() => {
+      const error = new Error();
+      error.stack = null;
+      throw error;
+    });
+
+    const httpResponse = await sut.handle(makeFakeRequest());
+    expect(httpResponse).toEqual(serverError(new ServerError(null)));
   });
 });
