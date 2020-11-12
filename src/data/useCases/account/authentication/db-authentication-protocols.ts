d173f28fb148da68f020ebfc1bad18c042ff328b
Diogo Pinheiro <diogo.fgomes.pinheiro@gmail.com>
2020-11-12 08:46:19 +0000

    refactor: change criptography folder name

--- src/data/useCases/account/authentication/db-authentication-protocols.ts
+++ src/data/useCases/account/authentication/db-authentication-protocols.ts
@@ -1,5 +1,5 @@
-export * from '@/data/protocols/crytography/hash-comparer';
-export * from '@/data/protocols/crytography/encrypter';
+export * from '@/data/protocols/criptography/hash-comparer';
+export * from '@/data/protocols/criptography/encrypter';
 export * from '@/data/protocols/db/account/load-account-by-email-repository';
 export * from '@/data/protocols/db/account/update-access-token-repository';
 export * from '@/domain/models/account';
