0fdde51c9f6b40f7a3136978c23276e3ac9bae5d
Diogo Pinheiro <diogo.fgomes.pinheiro@gmail.com>
2020-11-18 09:02:53 +0000

    test(data): ensure DbLoadSurveyResult throws if LoadSurveyResultRepository throws

--- src/data/useCases/survey-result/load-survey-result/db-load-survey-result.spec.ts
+++ src/data/useCases/survey-result/load-survey-result/db-load-survey-result.spec.ts
@@ -1,4 +1,5 @@
 import { mockLoadSurveyResultRepository } from '@/data/test';
+import { throwError } from '@/domain/test/test-helper';
 import { DbLoadSurveyResult } from './db-load-survey-result';
 import { LoadSurveyResultRepository } from './db-load-survey-result-protocols';
 
@@ -23,5 +24,15 @@ describe('DbLoadSurveyResult use case', () => {
 
     await sut.load('any_survey_id');
     expect(loadBySurveyIdSpy).toHaveBeenLastCalledWith('any_survey_id');
+  });
+
+  it('should throw if LoadSurveyResultRepository throws', async () => {
+    const { sut, loadSurveyResultRepositoryStub } = makeSut();
+    jest
+      .spyOn(loadSurveyResultRepositoryStub, 'loadBySurveyId')
+      .mockImplementationOnce(throwError);
+
+    const promise = sut.load('any_survey_id');
+    await expect(promise).rejects.toThrow();
   });
 });
