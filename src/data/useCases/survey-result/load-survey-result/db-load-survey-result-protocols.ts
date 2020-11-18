81a46eecb8210133d3f4f16f42d28f942a8177bf
Diogo Pinheiro <diogo.fgomes.pinheiro@gmail.com>
2020-11-18 08:58:39 +0000

    feat(data): ensure DbLoadSurveyResult calls LoadSurveyResultRepository with correct values

---
+++ src/data/useCases/survey-result/load-survey-result/db-load-survey-result-protocols.ts
@@ -0,0 +1,3 @@
+export * from '@/data/protocols/db/survey-result/load-survey-result-repository';
+export * from '@/domain/useCases/survey-result/load-survey-results';
+export * from '@/domain/models/survey-result';
