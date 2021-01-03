cd6ec2bf91093b196b22eb310c3da33b30fde062
Diogo Pinheiro <diogo.fgomes.pinheiro@gmail.com>
2021-01-03 00:26:45 +0000

    feat: add adapter for apollo server

---
+++ src/main/adapters/apollo-server-resolver-adapter.ts
@@ -0,0 +1,27 @@
+import {
+  UserInputError,
+  AuthenticationError,
+  ForbiddenError,
+  ApolloError
+} from 'apollo-server-express';
+import { Controller } from '@/presentation/protocols';
+
+export const adaptResolver = async (
+  controller: Controller,
+  args: any
+): Promise<any> => {
+  const httpResponse = await controller.handle(args);
+  switch (httpResponse.statusCode) {
+    case 200:
+    case 204:
+      return httpResponse.body;
+    case 400:
+      throw new UserInputError(JSON.stringify(httpResponse.body));
+    case 401:
+      throw new AuthenticationError(JSON.stringify(httpResponse.body));
+    case 403:
+      throw new ForbiddenError(JSON.stringify(httpResponse.body));
+    default:
+      throw new ApolloError(JSON.stringify(httpResponse.body));
+  }
+};
