3326c823fb5a969931c995440a08e4581db60e85
Diogo Pinheiro <diogo.fgomes.pinheiro@gmail.com>
2021-01-03 01:27:08 +0000

    feat: add surveys schema

--- src/main/graphql/resolvers/index.ts
+++ src/main/graphql/resolvers/index.ts
@@ -1,3 +1,5 @@
+import base from './base';
 import auth from './auth';
+import surveys from './surveys';
 
-export default [auth];
+export default [base, auth, surveys];
--- src/main/graphql/type-defs/index.ts
+++ src/main/graphql/type-defs/index.ts
@@ -1,4 +1,5 @@
 import base from './base';
 import auth from './auth';
+import surveys from './surveys';
 
-export default [base, auth];
+export default [base, auth, surveys];
