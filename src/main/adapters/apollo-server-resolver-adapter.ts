61225208052692740dfd48b3191b8623b20466f7
Diogo Pinheiro <diogo.fgomes.pinheiro@gmail.com>
2021-01-03 02:19:53 +0000

    feat: add surveyResult query and mutation

--- src/main/adapters/apollo-server-resolver-adapter.ts
+++ src/main/adapters/apollo-server-resolver-adapter.ts
@@ -8,9 +8,10 @@ import { Controller } from '@/presentation/protocols';
 
 export const adaptResolver = async (
   controller: Controller,
-  args?: any
+  args?: any,
+  context?: any
 ): Promise<any> => {
-  const request = { ...(args || {}) };
+  const request = { ...(args || {}), accountId: context?.req?.accountId };
   const httpResponse = await controller.handle(request);
   switch (httpResponse.statusCode) {
     case 200:
--- src/main/graphql/type-defs/index.ts
+++ src/main/graphql/type-defs/index.ts
@@ -1,5 +1,6 @@
 import base from './base';
 import auth from './auth';
 import surveys from './surveys';
+import surveyResults from './survey-results';
 
-export default [base, auth, surveys];
+export default [base, auth, surveys, surveyResults];
